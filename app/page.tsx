764fca1e38f84bf2b60e7539e49c670b70257381
jackkurian044-maker <jackkurian044@gmail.com>
2026-07-28 22:37:49 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <main style={{padding:"40px",fontFamily:"Arial"}}>
-      <h1>🚀 Stall Platform</h1>
-      <p>Phase 1 setup completed.</p>
-      <p>Next phase: Firebase Integration.</p>
-    </main>
+    <>
+      <Navbar />
+      <Hero />
+    </>
   );
 }
