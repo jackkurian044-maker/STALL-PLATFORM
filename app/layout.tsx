613b5aea211ff6e13c5ccf54d335e7fcd6a7cf9e
jackkurian044-maker <jackkurian044@gmail.com>
2026-07-28 22:38:13 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import "./globals.css";
+import type { Metadata } from "next";
 
-export const metadata = {
-  title: "Stall",
-  description: "Discover Local Businesses"
+export const metadata: Metadata = {
+  title: "STALL",
+  description: "Discover Local Businesses Around You",
 };
 
-export default function RootLayout({children}:{children:React.ReactNode}) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
       <body>{children}</body>
