aa339f02f469739285021351b6028ca9b5f7b303
jackkurian044-maker <jackkurian044@gmail.com>
2026-09-19 13:44:24 +0530

    Provide Prisma access to acquisition import workflow

--- apps/api/src/acquisition/acquisition.module.ts
+++ apps/api/src/acquisition/acquisition.module.ts
@@ -2,8 +2,10 @@ import { Module } from "@nestjs/common";
 import { AcquisitionController } from "./acquisition.controller";
 import { AcquisitionRepository } from "./acquisition.repository";
 import { AcquisitionService } from "./acquisition.service";
+import { PrismaModule } from "../prisma/prisma.module";
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AcquisitionController],
   providers: [AcquisitionRepository, AcquisitionService],
 })
