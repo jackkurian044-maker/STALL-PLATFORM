549981c3e854ca862f1fb79e807f7b470e367503
jackkurian044-maker <jackkurian044@gmail.com>
2026-09-19 13:32:13 +0530

    Register acquisition module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,10 +3,11 @@ import { PrismaModule } from "./prisma/prisma.module";
 import { BusinessModule } from "./business/business.module";
 import { CategoryModule } from "./category/category.module";
 import { ReviewModule } from "./review/review.module";
+import { AcquisitionModule } from "./acquisition/acquisition.module";
 import { AppController } from "./app.controller";
 
 @Module({
-  imports: [PrismaModule, BusinessModule, CategoryModule, ReviewModule],
+  imports: [PrismaModule, BusinessModule, CategoryModule, ReviewModule, AcquisitionModule],
   controllers: [AppController],
 })
 export class AppModule {}
